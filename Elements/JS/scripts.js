5f74c85949e564390432a44c86957524387816ee
Mujahid Hossain <opee.home@outlook.com>
2024-07-01 00:58:32 +0600

    Update scripts.js

--- Elements/JS/scripts.js
+++ Elements/JS/scripts.js
@@ -1,1 +1,10 @@
+const wrapper - document.querrySelector('.wrapper');
+const loginLink - document.querrySelector('.login link');
+const registerLink - document.querrySelector('.register link');
 
+registerLink.addEventListener('click', ()=> {
+  wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+  wrapper.classList.remove('active');
